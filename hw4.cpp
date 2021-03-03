71175c3e79276fd43505ffff7a4737ec699c894a
Stiven <sdiskein@ya.ru>
2021-03-03 16:21:32 +0300

    Task 2 is done

--- hw4.cpp
+++ hw4.cpp
@@ -11,5 +11,17 @@ int main(int argc, char* argv[]) {
 	sum = x + y;
 	if (sum >= 10 && sum <= 20) std::cout << "True\n";
 	else std::cout << "False\n";
+	//Task 2. Написать программу, проверяющую, 
+	//является ли некоторое число - натуральным простым.
+	int n = 757;
+	bool is_prime = 1;
+	if (n % 2 == 0) is_prime = 0;	//отбрасываем четные
+	for (int i = 3; i < n; i += 2){
+		if (n % i == 0){
+			is_prime = 0;			//есть делитель
+			break;					//нет смысла дальше проверять
+		}
+	}
+	std::cout << is_prime << "\n";
 	return 0;
 }
