0d25ed5bac5cf32af6692014a08351a2f759df4e
Stiven <sdiskein@ya.ru>
2021-03-05 12:32:13 +0300

    Task 1 is done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	return 0;
+int main() {
+    //Task 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+    // Написать функцию, заменяющую в принятом массиве 0 на 1, 1 на 0
+    // (** без применения if-else, switch, () ? :);
+    int testArray[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+    for (int i = 0; i < 10; i++){
+        testArray[i] = !testArray[i];       //or boolArray[i] ^ 1;
+        std::cout << testArray[i] << " ";
+    }
+    std::cout << "\n";
+    return 0;
 }
