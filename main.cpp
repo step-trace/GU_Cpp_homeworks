23f59950e887f6266f10b3a7e38e58aaffba790e
Stiven <sdiskein@ya.ru>
2021-03-05 12:55:05 +0300

    Task 2 is done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+void fillArray(int size, int *arr){
+    for (int i = 1; i <= size; ++i) {
+        arr[i-1] = i*3 - 2;
+    }
+}
+
 int main() {
     //Task 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
     // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
@@ -10,6 +16,15 @@ int main() {
         testArray[i] = !testArray[i];       //or boolArray[i] ^ 1;
         std::cout << testArray[i] << " ";
     }
-    std::cout << "\n";
+    std::cout <<"\n";
+    //Task 2. Задать пустой целочисленный массив размером 8.
+    // Написать функцию, которая с помощью цикла
+    // заполнит его значениями 1 4 7 10 13 16 19 22;
+    int toFillArray[8];
+    fillArray(8, toFillArray);
+    for (int i = 0; i < 8; ++i) {
+        std::cout << toFillArray[i] << "  ";
+    }
+    std::cout <<"\n";
     return 0;
 }
