9adef6eb17c0ea6cf0b131a7fc0e0a5dcde4e25b
Stiven <sdiskein@ya.ru>
2021-03-16 09:36:45 +0300

    Task 1 is done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#define CHECK_RANGE(a, b) ((((a) >= 0) && ((a) < (b))) ? true : false)
+
 int main(int argc, char* argv[]) {
+	std::cout << CHECK_RANGE(5 , 10) << CHECK_RANGE(5 + 10 , 10 + 5);
 	return 0;
 }
