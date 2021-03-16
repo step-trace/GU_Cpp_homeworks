ec4f8fc92ef4a77a9bad12e2485a884c172d5e93
Stiven <sdiskein@ya.ru>
2021-03-16 10:16:53 +0300

    Task 3 is done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #define ARRAY_SIZE 5
 #define CHECK_RANGE(a, b) ((((a) >= 0) && ((a) < (b))) ? true : false)
 #define GET_ELEMENT(arr_name, i, j) (*(*((arr_name) + (i)) + (j)))
+#define N_ELEMENTS(arr_name, type) ((sizeof(arr_name)) / (sizeof(type)))
 
 int main(int argc, char* argv[]) {
 	std::cout << CHECK_RANGE(5 , 10) << CHECK_RANGE(5 + 10 , 10 + 5) << std::endl;
@@ -19,5 +20,8 @@ int main(int argc, char* argv[]) {
 	for (int i = 0; i < ARRAY_SIZE; ++i)
 		for (int j = 0; j < ARRAY_SIZE; ++j)
 			std::cout << GET_ELEMENT(arr, i, j);
+	delete arr;
+	int lArray[10][3][3];
+	std::cout << std::endl << N_ELEMENTS(lArray, int) << std::endl;
 	return 0;
 }
